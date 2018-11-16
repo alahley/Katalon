8f6979fba694f40c672ede15647f8bfa646c8bcf
Adam Lahley <alahley@kollective.com>
2018-11-16 12:11:39 -0800

    Added new urls to Prod 3 Load Test

--- Test Suites/Prod 3 Sanity Check.ts
+++ Test Suites/Prod 3 Sanity Check.ts
@@ -4,7 +4,7 @@
    <name>Prod 3 Sanity Check</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-15T13:33:16</lastRun>
+   <lastRun>2018-11-16T11:39:19</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
