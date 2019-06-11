c8d8951d6798a61d75d28e72ddb856cca9e1b16f
Adam Lahley <alahley@kollective.com>
2019-06-11 15:19:36 -0700

    new test suite for kade

---
+++ Test Suites/Hobin Smoke Test.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This is a sanity check that Kade can fire off to ensure that all main parts of Kollective IQ are working.  Basic functionality that Kade can report is working, with no unexpected results:
+-Login/Logout
+-Navigation interactions
+-Main dashboard data (Summary pages)</description>
+   <name>Hobin Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e756c31d-dbd0-4c9a-87b2-ac6e33765142</testSuiteGuid>
+</TestSuiteEntity>
